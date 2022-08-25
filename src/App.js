5f507ed264f608d56ccffd1cb0cb3edcd780d604
diamond-nicholas <diamondnicholas154@gmail.com>
2022-08-25 01:14:52 +0100

    add ui for selected btns

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+// import installedSdks from './Datasets/installed.json';
 
 function App() {
   return (
-    <div>
-      <p>Hello world</p>
+    <div className='userPage'>
+      <h3>Welcome to airnow-fe-tech-test</h3>
+
+      <section className='btn-selector'>
+        <div className='wrapper'>
+          <button>Installed</button>
+          <div className='line'></div>
+          <button>Unistalled</button>
+        </div>
+      </section>
     </div>
   );
 }
